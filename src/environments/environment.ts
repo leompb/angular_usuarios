1e84929234857ce85590b1c358dee8533f6b1a5c
leompb <leompb@gmail.com>
2022-12-17 12:26:14 -0300

    first commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,9 @@
+export const environment = {
+    /* endereço base da API */
+    //API_URL: 'http://localhost:8080'
+    API_URL: 'http://apiusuarios-env.eba-qwym3m9c.us-east-1.elasticbeanstalk.com'
+}
+
+
+
+
